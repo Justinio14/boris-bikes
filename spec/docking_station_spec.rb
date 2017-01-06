c378bf06edf56b3d85ac81ac3fcec7c6127b0fe7
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-06 12:22:17 +0000

    Added feature to change amount of the capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,5 @@
 require "docking_station"
 
-
   describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
@@ -9,31 +8,29 @@ require "docking_station"
     subject.dock_bike(bike)
    bike = subject.release_bike
    expect(bike).to be_working
-
   end
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
   it 'docks something' do
-   bike = Bike.new
-   #subject.dock_bike(bike)
-   # We want to return the bike we dock
+    bike = Bike.new
+    #subject.dock_bike(bike)
+    # We want to return the bike we dock
    expect(subject.dock_bike(bike)).to eq [bike]
- end
+  end
 
   it "gives an error when docking station is full" do
-  DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(Bike.new) }
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(Bike.new) }
     expect{subject.dock_bike(Bike.new)}.to raise_error 'Full dock'
   end
 
-   describe '#release_bike' do
+  describe '#release_bike' do
    it 'releases a bike' do
      bike = Bike.new
      subject.dock_bike(bike)
      # we want to release the bike we docked
      expect(subject.release_bike).to eq bike
    end
-
 
    describe '#release_bike' do
      it 'raises an error if dock empty' do
@@ -42,7 +39,7 @@ require "docking_station"
    end
  end
 
-  describe '#capacity' do
+ describe '#capacity' do
    it 'Allow user to set capacity of docking station' do
      # Create a new station with 50 bikes and test
      num = 50
@@ -52,11 +49,9 @@ require "docking_station"
      # Create a default station using the DEFAULT_CAPACITY constant
      station = DockingStation.new
      expect(station.capacity).to eq DockingStation::DEFAULT_CAPACITY
-   end
-
+    end
   end
-
- end
+end
 
 
 
